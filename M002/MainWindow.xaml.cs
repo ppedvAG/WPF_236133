4b4eed8ec66b839ebccbcf0e11456c14494e4a32
ppedvLukasK <lukask@ppedv.de>
2024-01-30 11:48:40 +0100

    M001, M002

---
+++ M002/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace M002;
+
+public partial class MainWindow : Window
+{
+	public MainWindow()
+	{
+		InitializeComponent();
+	}
+
+	private void CheckBox_Checked(object sender, RoutedEventArgs e)
+	{
+		if (IsInitialized)
+		{
+			TB.Text = "Checked";
+		}
+	}
+
+	//private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+	//{
+	//	TB.FontSize = e.NewValue;
+	//}
+}
