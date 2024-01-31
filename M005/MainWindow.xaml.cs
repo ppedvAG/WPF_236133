a15aed6e9f0cf5ced1c1a82cfe68e491444b9714
ppedvLukasK <lukask@ppedv.de>
2024-01-31 10:08:05 +0100

    M005

---
+++ M005/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace M005;
+
+public partial class MainWindow : Window
+{
+	public Person p { get; set; } //Hier muss auch INotifyPropertyChanged implementiert werden
+
+	public BindableProperty<Person> p2 { get; set; } = new();
+
+	public ObservableCollection<Person> people { get; set; } = new();
+
+	public MainWindow()
+	{
+		p = new Person() { Name = "Max", Alter = 33 };
+		InitializeComponent();
+	}
+
+	private void Button_Click(object sender, RoutedEventArgs e)
+	{
+		p.Alter++;
+		p2.Value = new Person() { Name = "Max2", Alter = Random.Shared.Next() };
+		people.Add(new Person() { Name = new string(Random.Shared.Next().ToString().Select(e => (char) (e + 49)).ToArray()), Alter = Random.Shared.Next() });
+	}
+}
