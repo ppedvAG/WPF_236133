2146f2c9e64356672839961226484c0fdb45cdec
ppedvLukasK <lukask@ppedv.de>
2024-02-01 14:50:36 +0100

    M009

---
+++ M009/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace M009;
+
+
+public partial class MainWindow : Window
+{
+	public BindableProperty<string> TextContainer { get; set; } = new();
+
+	public MainWindow()
+	{
+		InitializeComponent();
+	}
+
+	private void Button_Click(object sender, RoutedEventArgs e)
+	{
+		TV.ItemsSource = new string[] { "T1", "T2", "T3" };
+	}
+
+	private void Button_Click_1(object sender, RoutedEventArgs e)
+	{
+		TV.ItemsSource = null;
+	}
+}
